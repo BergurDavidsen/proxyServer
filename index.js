4e4df7ac1dd1e5f071c4bc5ec295ac3237d4da2e
BergurDavidsen <bergurdavidsen@gmail.com>
2023-07-15 15:19:29 +0100

    test: just testing again with new flowtype

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ app.post('/proxy', express.text(), (req, res) => {
     'Content-Type': 'application/json'
   };
 
-  axios.post('https://webhook.api.staging.flowcore.io/event/bergurdavidsen/b23f801d-76d4-49fa-a52e-711088c795cb/running-data/create?key=4b882a56-5922-4db3-ac8f-603d848c7aa8', jsonData, { headers })
+  axios.post('https://webhook.api.staging.flowcore.io/event/bergurdavidsen/b23f801d-76d4-49fa-a52e-711088c795cb/proxy-data/create?key=4b882a56-5922-4db3-ac8f-603d848c7aa8', jsonData, { headers })
     .then(response => {
       console.log('Data sent to webhook successfully');
+      console.log(jsonData)
+
       res.sendStatus(200);
     })
     .catch(error => {
