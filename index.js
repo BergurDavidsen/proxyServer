27fdef50c301059879bafe63761cf55217bae1e6
BergurDavidsen <bergurdavidsen@gmail.com>
2023-07-15 15:47:47 +0100

    revert: going back to original

--- index.js
+++ index.js
@@ -8,18 +8,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
-app.post('/proxy', (req, res) => {
-  // Assuming the request body contains plain text data
-  const textData = req.body;
+app.post('/proxy', express.text(), (req, res) => {
+  const jsonData = JSON.parse(req.body);
 
-  let jsonData;
-  try {
-    // Attempt to parse the plain text data as JSON
-    jsonData = JSON.parse(textData);
-  } catch (error) {
-    console.error('Error parsing plain text data as JSON:', error);
-    return res.sendStatus(400);
-  }
+
 
   const headers = {
     'Content-Type': 'application/json'
@@ -28,7 +20,7 @@ app.post('/proxy', (req, res) => {
   axios.post('https://webhook.api.staging.flowcore.io/event/bergurdavidsen/b23f801d-76d4-49fa-a52e-711088c795cb/proxy-data/create?key=4b882a56-5922-4db3-ac8f-603d848c7aa8', jsonData, { headers })
     .then(response => {
       console.log('Data sent to webhook successfully');
-      console.log(jsonData);
+      console.log(jsonData)
 
       res.sendStatus(200);
     })
@@ -37,7 +29,6 @@ app.post('/proxy', (req, res) => {
       res.sendStatus(500);
     });
 });
-
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Proxy server listening on port ${port}`);
